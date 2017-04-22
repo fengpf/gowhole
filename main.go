14c8aad8d45a7b0add3568f8fe87563ae15363c5
fengpf <18221167541@163.com>
2017-04-22 13:11:55 +0800

    add signal

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "gostudy/imgprocessing"
+import (
+	"gostudy/imgprocessing"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
 func main() {
 	// err := imgprocessing.WaterMark("golang水印")
@@ -10,4 +16,21 @@ func main() {
 	// }
 	imgprocessing.HTTPPrint()
 	//fmt.Printf("%s\n", "create watermark success ...")
+	// signal handler
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT, syscall.SIGSTOP)
+	for {
+		s := <-c
+		println("get a signal")
+		switch s {
+		case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGSTOP, syscall.SIGINT:
+			time.Sleep(time.Second * 2)
+			println("exit")
+			return
+		case syscall.SIGHUP:
+		// TODO reload
+		default:
+			return
+		}
+	}
 }
