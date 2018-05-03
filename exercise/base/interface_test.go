f86b55347e69de792ec3b4e31891156f9e2336c7
fengpengfei <18221167541@163.com>
2018-05-03 23:52:36 +0800

    add waitgroup & sync.pool test

--- exercise/base/interface_test.go
+++ exercise/base/interface_test.go
@@ -3,12 +3,14 @@ package base
 import (
 	"bufio"
 	"bytes"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 	"strconv"
 	"strings"
 	"testing"
+	"time"
 	"unicode"
 )
 
@@ -123,4 +125,35 @@ func Test_scanWords(t *testing.T) {
 		fmt.Fprintln(os.Stderr, "reading input:", err)
 	}
 	fmt.Printf("%d\n", count)
+}
+
+func Test_flag(t *testing.T) {
+	var period = flag.Duration("period", 1*time.Second, "sleep period")
+	flag.Parse()
+	fmt.Printf("Sleeping for %v...\n", *period)
+	time.Sleep(*period)
+
+	var temp = CelsiusFlag("temp", 20.0, "the temperature")
+	fmt.Printf("temperature is %v...%b\n", *temp, 10)
+	fmt.Println()
+}
+
+var p = fmt.Println
+
+func Test_flag2(t *testing.T) {
+	p("Contains:  ", strings.Contains("test", "es"))
+	p("Count:     ", strings.Count("test", "t"))
+	p("HasPrefix: ", strings.HasPrefix("test", "te"))
+	p("HasSuffix: ", strings.HasSuffix("test", "st"))
+	p("Index:     ", strings.Index("test", "e"))
+	p("Join:      ", strings.Join([]string{"a", "b"}, "-"))
+	p("Repeat:    ", strings.Repeat("a", 5))
+	p("Replace:   ", strings.Replace("foo", "o", "0", -1))
+	p("Replace:   ", strings.Replace("foo", "o", "0", 1))
+	p("Split:     ", strings.Split("a-b-c-d-e", "-"))
+	p("ToLower:   ", strings.ToLower("TEST"))
+	p("ToUpper:   ", strings.ToUpper("test"))
+	p()
+	p("Len: ", len("hello"))
+	p("Char:", "hello"[1])
 }
--- exercise/base/string_test.go
+++ exercise/base/string_test.go
@@ -9,26 +9,6 @@ import (
 	"unsafe"
 )
 
-var p = fmt.Println
-
-func Test_flag(t *testing.T) {
-	p("Contains:  ", strings.Contains("test", "es"))
-	p("Count:     ", strings.Count("test", "t"))
-	p("HasPrefix: ", strings.HasPrefix("test", "te"))
-	p("HasSuffix: ", strings.HasSuffix("test", "st"))
-	p("Index:     ", strings.Index("test", "e"))
-	p("Join:      ", strings.Join([]string{"a", "b"}, "-"))
-	p("Repeat:    ", strings.Repeat("a", 5))
-	p("Replace:   ", strings.Replace("foo", "o", "0", -1))
-	p("Replace:   ", strings.Replace("foo", "o", "0", 1))
-	p("Split:     ", strings.Split("a-b-c-d-e", "-"))
-	p("ToLower:   ", strings.ToLower("TEST"))
-	p("ToUpper:   ", strings.ToUpper("test"))
-	p()
-	p("Len: ", len("hello"))
-	p("Char:", "hello"[1])
-}
-
 // Substr get part of a string.
 func Substr(str string, start, length int) string {
 	rs := []rune(str)
