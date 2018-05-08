8cd2416424b45bb7c62811d167d5b6c991603d16
fengpengfei <18221167541@163.com>
2018-05-08 17:39:31 +0800

    add timer test

--- exercise/basictests/time_test.go
+++ exercise/basictests/time_test.go
@@ -14,3 +14,18 @@ func Test_getCurTime(t *testing.T) {
 	c := time.Unix(time.Now().UnixNano()/1e9, 0) //将毫秒转换为 time 类型
 	fmt.Println(c.String())                      //输出当前英文时间戳格式
 }
+
+func Test_tick(t *testing.T) {
+	var timer = time.NewTicker(time.Duration(1) * time.Second)
+	var token = 0
+	go func() {
+		for range timer.C {
+			token++
+			fmt.Println(token)
+			if token == 20 {
+				break
+			}
+		}
+	}()
+	time.Sleep(5 * time.Second)
+}
