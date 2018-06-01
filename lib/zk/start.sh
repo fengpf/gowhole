ec69c63fd3d6883e4df6080f928773a634e8b17b
fpf <fengpengfei@bilibili.com>
2018-06-01 11:36:30 +0800

    add elec zk master

--- lib/zk/start.sh
+++ lib/zk/start.sh
@@ -1,6 +1,5 @@
-docker run -it --rm \
-        --link zoo1:zk1 \
-        --link zoo2:zk2 \
-        --link zoo3:zk3 \
-        --net zk_test_default \
-        zookeeper zkCli.sh -server zk1:2181,zk2:2181,zk3:2181
+
+echo "zk node start ..."
+
+COMPOSE_PROJECT_NAME=zk_test docker-compose up
+
