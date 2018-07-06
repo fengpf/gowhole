37e70f25265f5de189dbd023292d79031184448f
fpf <fengpengfei@bilibili.com>
2018-07-06 10:50:39 +0800

    add chan dealock

--- exercise/channel/tick/main.go
+++ exercise/channel/tick/main.go
@@ -2,14 +2,70 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-// func init() {
-// 	runtime.GOMAXPROCS(runtime.NumCPU())
-// }
+const (
+	count = 100
+)
+
+type sender struct {
+	myChan chan int
+	wg     sync.WaitGroup
+}
+
+var s *sender
+
+func init() {
+	// runtime.GOMAXPROCS(runtime.NumCPU())
+	s = &sender{
+		myChan: make(chan int, 1024),
+	}
+	for i := 0; i < count; i++ {
+		go func(i int) {
+			s.myChan <- i
+		}(i)
+	}
+}
 
 func main() {
+	s.wg.Add(1)
+	go s.send()
+	s.wg.Wait()
+}
+
+func (s *sender) send() {
+	defer s.wg.Done()
+	tick := time.NewTicker(1 * time.Second)
+	for {
+		select {
+		case <-tick.C:
+			sends := []int{}
+			var done bool
+			for {
+				if done {
+					break
+				} else if len(sends) == 10 {
+					fmt.Println("=10...")
+					break
+				}
+				select {
+				case data := <-s.myChan:
+					sends = append(sends, data)
+				default:
+					fmt.Println("done = true")
+					done = true
+				}
+			}
+			if len(sends) > 0 {
+				fmt.Println(sends)
+			}
+		}
+	}
+}
+
+func tick() {
 	ch := make(chan int, 1024)
 	go func(ch chan int) {
 		for {
