2928b0865ad0220c99a6c0c7cdaa5b3cf8acf796
fpf <fengpengfei@bilibili.com>
2018-07-25 19:23:23 +0800

    add memory manager study

---
+++ exercise/runtime/memory/allocate/heap/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+func main() {
+	test := struct{}{}
+	fmt.Println(unsafe.Sizeof(test)) //0
+
+	var in interface{}
+	fmt.Println(unsafe.Sizeof(in)) //16 由于接口自身数据结构是由两个指针构成
+
+	//有个例外，就是0长度作为结构体的最后一个成员
+	var x struct {
+		a int
+		b struct{}
+	}
+	fmt.Println(unsafe.Sizeof(x)) //16
+
+	//go build -gcflags '-l' -o  main main.go
+	//go tool objdump -s "main\.main" main
+	// go 不支持指针运算和转型，只要有个合法的地址就ok，反正0长度不能赋值(忽略)和越界。unsafe除外
+	//如果是堆上分配，所有0长度都是指向一个全局变量，共享同一个内存地址，就算类型相同也一样
+	var (
+		a struct{}
+		b int = 0x100
+		c     = new(struct{})
+		d     = new([0]int)
+	)
+	_ = a
+	_ = b
+	_ = c
+	_ = d
+	println(&a, &b) //0xc42004bf30 0xc42004bf30  相同地址
+	println(&c, &d) //0xc42004bf40 0xc42004bf38 不同地址
+	// fmt.Printf("%p\t%p\n", &a, &b) //使用fmt.Printf,地址不同，原因是参数是interface,会导致堆上分配，所以会导致ab地址不同
+	// fmt.Printf("%p\t%p\n", &c, &d) //确保在堆上分配
+}
