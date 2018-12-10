7f4b17db607f7dc43302ed5921d2c9f9e72b8f32
fengpf <18221167541@163.com>
2018-12-10 10:42:04 +0800

    add flow

---
+++ middleware/flow/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"strings"
+
+	"github.com/chrislusf/glow/flow"
+
+	_ "github.com/chrislusf/glow/driver"
+)
+
+func main() {
+	flag.Parse()
+
+	flow.New().TextFile(
+		"/etc/passwd", 3,
+	).Filter(func(line string) bool {
+		return !strings.HasPrefix(line, "#")
+	}).Map(func(line string, ch chan string) {
+		for _, token := range strings.Split(line, ":") {
+			ch <- token
+		}
+	}).Map(func(key string) int {
+		return 1
+	}).Reduce(func(x int, y int) int {
+		return x + y
+	}).Map(func(x int) {
+		println("count:", x)
+	}).Run()
+}
